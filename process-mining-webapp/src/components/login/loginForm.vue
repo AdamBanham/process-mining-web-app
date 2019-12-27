8980a3ead225f71b7761ec941a16c59da31120ca
Adam Peter Banham <n7176546@qut.edu.au>
2019-12-27 00:22:09 +0000

    created login form component

---
+++ process-mining-webapp/src/components/login/loginForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login-form">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: "loginForm",
+    components: {
+
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
