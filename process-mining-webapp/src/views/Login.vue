295577c3f71d019faeee0abf7afda415a68b7a59
Adam Peter Banham <n7176546@qut.edu.au>
2019-12-27 00:20:34 +0000

    created login view

---
+++ process-mining-webapp/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+      <loginForm />
+  </div>
+</template>
+
+<script>
+import loginForm from '../components/login/loginForm'
+export default {
+    name:"login",
+    components: {
+        loginForm
+    },
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
