f607f696db194d0b84d48969851264f82136d855
Adam Peter Banham <n7176546@qut.edu.au>
2019-12-27 00:36:19 +0000

    updated login form to have fields

--- process-mining-webapp/src/components/login/loginForm.vue
+++ process-mining-webapp/src/components/login/loginForm.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="login-form">
-      
+      <b-field label="Username" class="login-field">
+          <b-input v-model="username" />
+      </b-field>
+      <b-field label="Password" class="login-field">
+          <b-input v-model="password" />
+      </b-field>
+      <b-button @click="login" class='is-info login-button'>Login</b-button>
   </div>
 </template>
 
@@ -9,10 +15,20 @@ export default {
     name: "loginForm",
     components: {
 
+    },
+    data() {
+        return {
+            password : null,
+            username : null,
+        }
     }
 }
 </script>
 
 <style lang="sass" scoped>
-
+.login-button
+    width: 150px;
+.login-field
+    margin-left: 3%;
+    margin-right: 3%;
 </style>
